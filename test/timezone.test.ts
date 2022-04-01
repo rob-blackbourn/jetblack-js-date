649a3260fe114f36f01d46e299fabf5769e71f1e
Rob Blackbourn <rob.blackbourn@gmail.com>
2022-04-01 14:01:32 +0100

    Moved tzdata to json file

--- test/timezone.test.ts
+++ test/timezone.test.ts
@@ -1,91 +1,17 @@
 import { Duration, CustomTimezone, TimezoneDelta } from '../src'
+import timezones from './timezones.json'
 
-const tzBrussels = new CustomTimezone('Europe/Brussels', [
-  {
-    utc: new Date('1999-03-28T00:59:59.00Z'),
-    local: new Date('1999-03-28T01:59:59.00Z'),
-    offset: new Duration('PT1H'),
-    abbr: 'CET',
-    isDst: false
-  },
-  {
-    utc: new Date('1999-03-28T01:00:00.00Z'),
-    local: new Date('1999-03-28T03:00:00.00Z'),
-    offset: new Duration('PT2H'),
-    abbr: 'CEST',
-    isDst: true
-  },
-  {
-    utc: new Date('1999-10-31T00:59:59.00Z'),
-    local: new Date('1999-10-31T02:59:59.00Z'),
-    offset: new Duration('PT2H'),
-    abbr: 'CEST',
-    isDst: true
-  },
-  {
-    utc: new Date('1999-10-31T01:00:00.00Z'),
-    local: new Date('1999-10-31T02:00:00.00Z'),
-    offset: new Duration('PT1H'),
-    abbr: 'CET',
-    isDst: false
-  },
-  {
-    utc: new Date('2000-03-26T00:59:59.00Z'),
-    local: new Date('2000-03-26T01:59:59.00Z'),
-    offset: new Duration('PT1H'),
-    abbr: 'CET',
-    isDst: false
-  },
-  {
-    utc: new Date('2000-03-26T01:00:00.00Z'),
-    local: new Date('2000-03-26T03:00:00.00Z'),
-    offset: new Duration('-PT2H'),
-    abbr: 'CEST',
-    isDst: true
-  },
-  {
-    utc: new Date('2000-10-29T00:59:59.00Z'),
-    local: new Date('2000-10-29T02:59:59.00Z'),
-    offset: new Duration('PT2H'),
-    abbr: 'CEST',
-    isDst: true
-  },
-  {
-    utc: new Date('2000-10-29T01:00:00.00Z'),
-    local: new Date('2000-10-29T02:00:00.00Z'),
-    offset: new Duration('PT1H'),
-    abbr: 'CET',
-    isDst: false
-  },
-  {
-    utc: new Date('2001-03-25T00:59:59.00Z'),
-    local: new Date('2001-03-25T01:59:59.00Z'),
-    offset: new Duration('PT1H'),
-    abbr: 'CET',
-    isDst: false
-  },
-  {
-    utc: new Date('2001-03-25T01:00:00.00Z'),
-    local: new Date('2001-03-25T03:00:00.00Z'),
-    offset: new Duration('PT2H'),
-    abbr: 'CEST',
-    isDst: true
-  },
-  {
-    utc: new Date('2001-10-28T00:59:59.00Z'),
-    local: new Date('2001-10-28T02:59:59.00Z'),
-    offset: new Duration('PT2H'),
-    abbr: 'CEST',
-    isDst: true
-  },
-  {
-    utc: new Date('2001-10-28T01:00:00.00Z'),
-    local: new Date('2001-10-28T02:00:00.00Z'),
-    offset: new Duration('PT1H'),
-    abbr: 'CET',
-    isDst: false
-  }
-])
+const BRUSSELS_TZNAME = 'Europe/Brussels'
+const tzBrussels = new CustomTimezone(
+  BRUSSELS_TZNAME,
+  timezones['Europe/Brussels'].map(delta => ({
+    utc: new Date(delta.utc),
+    local: new Date(delta.local),
+    offset: new Duration(delta.offset),
+    abbr: delta.abbr,
+    isDst: delta.isDst
+  }))
+)
 
 describe('timezone', () => {
   describe('basic', () => {
