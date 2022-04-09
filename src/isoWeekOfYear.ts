b170e85c570950b143b8c1e012cc7bce6f716005
Rob Blackbourn <rob.blackbourn@gmail.com>
2022-04-09 08:45:43 +0100

    Added a doc comment

--- src/isoWeekOfYear.ts
+++ src/isoWeekOfYear.ts
@@ -4,6 +4,13 @@ import { tzLocal } from './LocalTimezone'
 import { startOfYear } from './startOfYear'
 import { Timezone } from './Timezone'
 
+/**
+ * Find the ISO week of the year.
+ *
+ * @param date The date.
+ * @param tz An optional timezone. Defaults to the local timezone.
+ * @returns The ISO week.
+ */
 export function isoWeekOfYear(date: Date, tz: Timezone = tzLocal): number {
   const nearestThursday = addDays(date, 4 - (tz.weekday(date) || 7))
   const yearStart = startOfYear(nearestThursday, tz)
