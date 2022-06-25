26783a7c41f62bd86f678c27ef306141bf414e2f
Rob Blackbourn <rob.blackbourn@gmail.com>
2022-06-25 13:46:41 +0100

    Added docs

--- src/HolidayCalendar.ts
+++ src/HolidayCalendar.ts
@@ -3,6 +3,27 @@ import { WeekendCalendar } from './WeekendCalendar'
 /**
  * A calendar class which supports weekends and an array of arbitrary dates.
  *
+ * The following constructs a holiday calendar for the US in 2020.
+ *
+ * ```js
+ * import { HolidayCalendar } from '@jetblack/date'
+ *
+ * const calUS = new HolidayCalendar(
+ *   'US',
+ *   [0, 6], // Saturday and Sunday are always holidays.
+ *   [
+ *     new Date('2020-01-01'), // Wed: New Year's Day
+ *     new Date('2020-01-20'), // Mon: Martin Luther King Jr. Day
+ *     new Date('2020-05-25'), // Mon: Memorial Day
+ *     new Date('2020-07-03'), // Fri: Independence Day
+ *     new Date('2020-09-07'), // Mon: Labor Day
+ *     new Date('2020-11-11'), // Wed: Veterans Day
+ *     new Date('2020-11-26'), // Thu: Thanksgiving
+ *     new Date('2020-12-25')  // Fri: Christmas Day
+ *   ]
+ * )
+ * ```
+ *
  * @category Calendars
  */
 export class HolidayCalendar extends WeekendCalendar {
