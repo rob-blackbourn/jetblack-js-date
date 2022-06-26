2766fcc0109629bafdb09646a426142a5175edc7
Rob Blackbourn <rob.blackbourn@gmail.com>
2022-06-26 08:46:57 +0100

    Added docs for addMonths

--- src/addMonths.ts
+++ src/addMonths.ts
@@ -4,6 +4,29 @@ import { Timezone } from './Timezone'
 /**
  * Add months to a date (or subtract if negative).
  *
+ * The day of the month is kept constant if possible. Where the destination
+ * has less days at the end of the month, the surplus days are added.
+ *
+ * ```js
+ * import { addMonths } from '@jetblack/date'
+ *
+ * // The day of the month is kept constant if possible.
+ * const d1 = new Date('2000-01-12')
+ * console.log(d1.toString())
+ * // Wed Jan 12 2000 00:00:00 GMT+0000 (Greenwich Mean Time)
+ * const d2 = addMonths(d1, 1)
+ * console.log(d2.toString())
+ * // Sat Feb 12 2000 00:00:00 GMT+0000 (Greenwich Mean Time)
+ *
+ * // If there are less days in the following month the surplus days are added.
+ * const d3 = new Date('2000-01-31')
+ * console.log(d3.toString())
+ * // Mon Jan 31 2000 00:00:00 GMT+0000 (Greenwich Mean Time)
+ * const d4 = addMonths(d3, 1)
+ * console.log(d4.toString())
+ * // Thu Mar 02 2000 00:00:00 GMT+0000 (Greenwich Mean Time)
+ * ```
+ *
  * @category Arithmetic
  *
  * @param date The start date.
