fb67f470c1b0005f5803613001b8f9fa397024e1
Rob Blackbourn <rob.blackbourn@gmail.com>
2022-06-26 11:46:53 +0100

    Added docs for addHours

--- src/addHours.ts
+++ src/addHours.ts
@@ -4,6 +4,19 @@ import { Timezone } from './Timezone'
 /**
  * Add hours to a date (or subtract if negative).
  *
+ * Daylight savings time is handled appropriately.
+ *
+ * ```js
+ * // The local timezone here is London.
+ * // In London on Sunday March 26 2000 the clocks go forward 1 hour.
+ * const d1 = new Date('2000-03-26T00:00:00')
+ * console.log(d1)
+ * // Sun Mar 26 2000 00:00:00 GMT+0000 (Greenwich Mean Time)
+ * const d2 = addHours(d1, 1, tzLocal)
+ * console.log(d2)
+ * // Sun Mar 26 2000 02:00:00 GMT+0100 (British Summer Time)
+ * ```
+ *
  * @category Arithmetic
  *
  * @param date The start date.
