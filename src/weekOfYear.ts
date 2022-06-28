ccd15d390bfa18029886fb1e2a7c1982defdd304
Rob Blackbourn <rob.blackbourn@gmail.com>
2022-06-28 18:06:15 +0100

    Simplify

--- src/weekOfYear.ts
+++ src/weekOfYear.ts
@@ -1,4 +1,5 @@
 import { tzLocal } from './LocalTimezone'
+import { startOfYear } from './startOfYear'
 import { Timezone } from './Timezone'
 
 const MILLISECONDS_IN_DAY = 24 * 60 * 60 * 1000
@@ -13,7 +14,7 @@ const MILLISECONDS_IN_DAY = 24 * 60 * 60 * 1000
  * @returns The week of the year.
  */
 export function weekOfYear(date: Date, tz: Timezone = tzLocal): number {
-  const jan1 = tz.makeDate(tz.year(date), 0, 1)
+  const jan1 = startOfYear(date, tz)
   const daysBetween = (date.getTime() - jan1.getTime()) / MILLISECONDS_IN_DAY
   return 1 + Math.trunc(daysBetween / 7)
 }
