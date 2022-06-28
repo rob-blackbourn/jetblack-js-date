b0d6015e756d9d2daf4f2d632fb6e69209177fba
Rob Blackbourn <rob.blackbourn@gmail.com>
2022-06-28 18:28:54 +0100

    Added docs

--- src/compareDateAsc.ts
+++ src/compareDateAsc.ts
@@ -3,6 +3,18 @@
  *
  * @category Comparisons
  *
+ * ```js
+ * const a = [
+ *   new Date('2000-07-01'),
+ *   new Date('2000-05-01'),
+ *   new Date('2000-06-01')
+ * ]
+ * console.log(a.sort(compareDateAsc))
+ * // 0: Mon May 01 2000 01:00:00 GMT+0100 (British Summer Time)
+ * // 1: Thu Jun 01 2000 01:00:00 GMT+0100 (British Summer Time)
+ * // 2: Sat Jul 01 2000 01:00:00 GMT+0100 (British Summer Time)
+ * ```
+ *
  * @param a The first date.
  * @param b The second date.
  * @returns 1 if the first date is greater than the second date, -1 if the first date is less than the last date, otherwise 0.
