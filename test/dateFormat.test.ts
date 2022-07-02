67280031a0f850a4f202da2b5d89bddc2220c422
Rob Blackbourn <rob.blackbourn@gmail.com>
2022-07-02 07:07:33 +0100

    started the tests

--- test/dateFormat.test.ts
+++ test/dateFormat.test.ts
@@ -6,4 +6,14 @@ describe('dateFormat', () => {
     const s = dateFormat(d, 'ddd, dd mmm yyyy HH:MM:ss Z', tzUtc)
     expect(s).toBe('Sat, 02 Jul 2022 06:39:15 UTC')
   })
+
+  it('should pad d when necessary', () => {
+    expect(dateFormat(new Date('2001-01-09T00:00:00Z'), 'd', tzUtc)).toBe('9')
+    expect(dateFormat(new Date('2001-01-19T00:00:00Z'), 'd', tzUtc)).toBe('19')
+  })
+
+  it('should always pad dd when necessary', () => {
+    expect(dateFormat(new Date('2001-01-09T00:00:00Z'), 'dd', tzUtc)).toBe('09')
+    expect(dateFormat(new Date('2001-01-19T00:00:00Z'), 'dd', tzUtc)).toBe('19')
+  })
 })
