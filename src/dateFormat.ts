01116c6ca63773b98f3fbeffc1be49391c215ba0
Rob Blackbourn <rob.blackbourn@gmail.com>
2022-07-03 11:08:50 +0100

    Added docs

--- src/dateFormat.ts
+++ src/dateFormat.ts
@@ -6,11 +6,6 @@
  * Includes enhancements by Scott Trenda <scott.trenda.net>
  * and Kris Kowal <cixar.com/~kris.kowal/>
  * and Rob Blackbourn.
- *
- * Accepts a date, a mask, or a date and a mask.
- * Returns a formatted version of the given date.
- * The date defaults to the current date/time.
- * The mask defaults to masks.default.
  */
 
 import { addDays } from './addDays'
@@ -19,29 +14,15 @@ import { tzLocal } from './LocalTimezone'
 import { Timezone } from './Timezone'
 import { tzUtc } from './UTCTimezone'
 
-// Regexes and supporting functions are cached through closure
+/** @internal  */
 const token =
   /d{1,4}|D{3,4}|m{1,4}|yy(?:yy)?|([HhMSTt])\1?|F{1,3}|W{1,2}|[opZN]|"[^"]*"|'[^']*'/g
-
-export let defaultMasks: { [name: string]: string } = {
-  default: 'ddd mmm dd yyyy HH:MM:SS',
-  shortDate: 'm/d/yy',
-  paddedShortDate: 'mm/dd/yyyy',
-  mediumDate: 'mmm d, yyyy',
-  longDate: 'mmmm d, yyyy',
-  fullDate: 'dddd, mmmm d, yyyy',
-  shortTime: 'h:MM TT',
-  mediumTime: 'h:MM:SS TT',
-  longTime: 'h:MM:SS TT Z',
-  isoDate: 'yyyy-mm-dd',
-  isoTime: 'HH:MM:SS',
-  isoDateTime: "yyyy-mm-dd'T'HH:MM:SSo",
-  expiresHeaderFormat: 'ddd, dd mmm yyyy HH:MM:SS Z'
-}
 
 /**
  * Get ISO-8601 numeric representation of the day of the week
  * 1 (for Monday) through 7 (for Sunday)
+ *
+ * @ internal
  *
  * @param date A date
  * @returns The ISO week day
@@ -51,26 +32,30 @@ const getDayOfWeek = (date: Date, tz: Timezone): number => {
   return dow === 0 ? 7 : dow
 }
 
+/** @internal  */
 interface LocaleNames {
   weekday: { short: Array<string | undefined>; long: Array<string | undefined> }
   month: { short: Array<string | undefined>; long: Array<string | undefined> }
 }
-type LocalNameType = 'weekday' | 'month'
-type LocalNameStyle = 'short' | 'long'
+/** @internal  */
+type LocaleOptionName = 'weekday' | 'month'
+/** @internal  */
+type LocaleOptionValue = 'short' | 'long'
+
+/** @internal  */
 const localeCache: { [locale: string]: LocaleNames } = {}
 
+/** @internal  */
 function getLocaleName(
   index: number,
-  type: LocalNameType,
-  style: LocalNameStyle,
+  type: LocaleOptionName,
+  style: LocaleOptionValue,
   locale: string | undefined
 ): string {
-  if (!locale) {
-    locale = Intl.DateTimeFormat().resolvedOptions().locale
-  }
+  const cacheName = !locale ? 'default' : locale
 
-  if (!(locale in localeCache)) {
-    localeCache[locale] = {
+  if (!(cacheName in localeCache)) {
+    localeCache[cacheName] = {
       weekday: {
         short: Array(7).fill(undefined),
         long: Array(7).fill(undefined)
@@ -81,13 +66,13 @@ function getLocaleName(
       }
     }
   }
-  let localeName = localeCache[locale][type][style][index]
+  let localeName = localeCache[cacheName][type][style][index]
   if (!localeName) {
     const monthIndex = type === 'month' ? index : 0
     const day = type === 'weekday' ? 1 + index : 1
     const date = tzUtc.makeDate(1967, monthIndex, day)
     localeName = date.toLocaleString(locale, { [type]: style })
-    localeCache[locale][type][style][index] = localeName
+    localeCache[cacheName][type][style][index] = localeName
   }
 
   return localeName
@@ -96,6 +81,9 @@ function getLocaleName(
 /**
  * Get day name
  * Yesterday, Today, Tomorrow if the date lies within, else fallback to Monday - Sunday
+ *
+ * @internal
+ *
  * @param  {Object}
  * @return {String}
  */
@@ -141,21 +129,61 @@ const getDayName = ({
   return dayName
 }
 
-export interface DateFormatOptions {
-  masks?: { [name: string]: string }
-  locale?: string
-}
-
+/**
+ * Format a date with a pattern.
+ *
+ * ```js
+ * const d = new Date("2000-01-01")
+ * const s = dateFormat(d, "d-mmm-yy")
+ * console.log(s)
+ * > 1-Jan-00
+ * ```
+ *
+ * | Value   | Description |
+ * | ------- | ----------- |
+ * | d       | Day of the month as digits; no leading zero for single-digit days. |
+ * | dd      | Day of the month as digits; leading zero for single-digit days. |
+ * | ddd     | Day of the week as a three-letter abbreviation. |
+ * | dddd    | Day of the week as its full name. |
+ * | m       | Month as digits; no leading zero for single-digit months. |
+ * | mm      | Month as digits; leading zero for single-digit months. |
+ * | mmm     | Month as a three-letter abbreviation. |
+ * | mmmm    | Month as its full name. |
+ * | yy      | Year as last two digits; leading zero for years less than 10. |
+ * | yyyy    | Year represented by four digits. |
+ * | h       | Hours; no leading zero for single-digit hours (12-hour clock). |
+ * | hh      | Hours; leading zero for single-digit hours (12-hour clock). |
+ * | H       | Hours; no leading zero for single-digit hours (24-hour clock). |
+ * | HH      | Hours; leading zero for single-digit hours (24-hour clock). |
+ * | M       | Minutes; no leading zero for single-digit minutes. |
+ * | MM      | Minutes; leading zero for single-digit minutes. |
+ * | S       | Seconds; no leading zero for single-digit seconds. |
+ * | SS      | Seconds; leading zero for single-digit seconds. |
+ * | F       | Hundreds of milliseconds. |
+ * | FF      | Tens of milliseconds with a leading zero for single-digit values. |
+ * | FFF     | Milliseconds: zero padded. |
+ * | t       | Lowercase, single-character time marker string: a or p. |
+ * | tt      | Lowercase, two-character time marker string: am or pm. |
+ * | T       | Uppercase, single-character time marker string: A or P. |
+ * | TT      | Uppercase, two-character time marker string: AM or PM. |
+ * | Z       | Timezone name. |
+ * | o       | GMT/UTC timezone offset, e.g. -0500 or +0230. |
+ * | '...' or "..." | Literal character sequence. Surrounding quotes are removed. |
+ *
+ * @category Formatting
+ *
+ * @param date A date.
+ * @param pattern The format pattern.
+ * @param tz An optional timezone. Defaults to the local timezone.
+ * @param locale An optional locale. Defaults to the current browser locale.
+ * @returns The formatted date string.
+ */
 export function dateFormat(
   date: Date,
-  mask: string,
-  options: DateFormatOptions,
-  tz: Timezone = tzLocal
+  pattern: string = "yyyy-mm-dd'T'HH:MM:SSo",
+  tz: Timezone = tzLocal,
+  locale: string | undefined = undefined
 ) {
-  const { masks = defaultMasks, locale } = options
-
-  const pattern = String(masks[mask] || mask)
-
   const d = () => tz.day(date)
   const D = () => tz.weekday(date)
   const m = () => tz.monthIndex(date)
--- test/dateFormat.test.ts
+++ test/dateFormat.test.ts
@@ -3,249 +3,154 @@ import { dateFormat, tzUtc, tzLocal } from '../src'
 describe('dateFormat', () => {
   it('should pass smoke test for en', () => {
     const d = new Date('2022-07-02T06:39:15.291Z')
-    const s = dateFormat(
-      d,
-      'ddd, dd mmm yyyy HH:MM:SS Z',
-      { locale: 'en' },
-      tzUtc
-    )
+    const s = dateFormat(d, 'ddd, dd mmm yyyy HH:MM:SS Z', tzUtc, 'en')
     expect(s).toBe('Sat, 02 Jul 2022 06:39:15 UTC')
   })
 
   it('should pass smoke test for fr', () => {
     const d = new Date('2022-07-02T06:39:15.291Z')
-    const s = dateFormat(
-      d,
-      'ddd, dd mmm yyyy HH:MM:SS Z',
-      { locale: 'fr' },
-      tzUtc
-    )
+    const s = dateFormat(d, 'ddd, dd mmm yyyy HH:MM:SS Z', tzUtc, 'fr')
     expect(s).toBe('sam., 02 juil. 2022 06:39:15 UTC')
   })
 
   it('should pad d when necessary', () => {
-    expect(
-      dateFormat(new Date('2001-01-09T00:00:00Z'), 'd', { locale: 'en' }, tzUtc)
-    ).toBe('9')
-    expect(
-      dateFormat(new Date('2001-01-19T00:00:00Z'), 'd', { locale: 'en' }, tzUtc)
-    ).toBe('19')
+    expect(dateFormat(new Date('2001-01-09T00:00:00Z'), 'd', tzUtc, 'en')).toBe(
+      '9'
+    )
+    expect(dateFormat(new Date('2001-01-19T00:00:00Z'), 'd', tzUtc, 'en')).toBe(
+      '19'
+    )
   })
 
   it('should always pad dd when necessary', () => {
     expect(
-      dateFormat(
-        new Date('2001-01-09T00:00:00Z'),
-        'dd',
-        { locale: 'en' },
-        tzUtc
-      )
+      dateFormat(new Date('2001-01-09T00:00:00Z'), 'dd', tzUtc, 'en')
     ).toBe('09')
     expect(
-      dateFormat(
-        new Date('2001-01-19T00:00:00Z'),
-        'dd',
-        { locale: 'en' },
-        tzUtc
-      )
+      dateFormat(new Date('2001-01-19T00:00:00Z'), 'dd', tzUtc, 'en')
     ).toBe('19')
   })
 
   it('should get ddd', () => {
     expect(
-      dateFormat(
-        new Date('2001-01-09T00:00:00Z'),
-        'ddd',
-        { locale: 'en' },
-        tzUtc
-      )
+      dateFormat(new Date('2001-01-09T00:00:00Z'), 'ddd', tzUtc, 'en')
     ).toBe('Tue')
   })
 
   it('should get dddd', () => {
     expect(
-      dateFormat(
-        new Date('2001-01-09T00:00:00Z'),
-        'dddd',
-        { locale: 'en' },
-        tzUtc
-      )
+      dateFormat(new Date('2001-01-09T00:00:00Z'), 'dddd', tzUtc, 'en')
     ).toBe('Tuesday')
   })
 
   it('should pad m when necessary', () => {
-    expect(
-      dateFormat(new Date('2001-09-09T00:00:00Z'), 'm', { locale: 'en' }, tzUtc)
-    ).toBe('9')
-    expect(
-      dateFormat(new Date('2001-10-19T00:00:00Z'), 'm', { locale: 'en' }, tzUtc)
-    ).toBe('10')
+    expect(dateFormat(new Date('2001-09-09T00:00:00Z'), 'm', tzUtc, 'en')).toBe(
+      '9'
+    )
+    expect(dateFormat(new Date('2001-10-19T00:00:00Z'), 'm', tzUtc, 'en')).toBe(
+      '10'
+    )
   })
 
   it('should always pad mm when necessary', () => {
     expect(
-      dateFormat(
-        new Date('2001-09-10T00:00:00Z'),
-        'mm',
-        { locale: 'en' },
-        tzUtc
-      )
+      dateFormat(new Date('2001-09-10T00:00:00Z'), 'mm', tzUtc, 'en')
     ).toBe('09')
     expect(
-      dateFormat(
-        new Date('2001-10-11T00:00:00Z'),
-        'mm',
-        { locale: 'en' },
-        tzUtc
-      )
+      dateFormat(new Date('2001-10-11T00:00:00Z'), 'mm', tzUtc, 'en')
     ).toBe('10')
   })
 
   it('should get mmm', () => {
     expect(
-      dateFormat(
-        new Date('2001-01-09T00:00:00Z'),
-        'mmm',
-        { locale: 'en' },
-        tzUtc
-      )
+      dateFormat(new Date('2001-01-09T00:00:00Z'), 'mmm', tzUtc, 'en')
     ).toBe('Jan')
   })
 
   it('should get mmmm', () => {
     expect(
-      dateFormat(
-        new Date('2001-01-09T00:00:00Z'),
-        'mmmm',
-        { locale: 'en' },
-        tzUtc
-      )
+      dateFormat(new Date('2001-01-09T00:00:00Z'), 'mmmm', tzUtc, 'en')
     ).toBe('January')
   })
 
   it('should get yy', () => {
     expect(
-      dateFormat(
-        new Date('2001-01-09T00:00:00Z'),
-        'yy',
-        { locale: 'en' },
-        tzUtc
-      )
+      dateFormat(new Date('2001-01-09T00:00:00Z'), 'yy', tzUtc, 'en')
     ).toBe('01')
   })
 
   it('should get yyyy', () => {
     expect(
-      dateFormat(
-        new Date('2001-01-09T00:00:00Z'),
-        'yyyy',
-        { locale: 'en' },
-        tzUtc
-      )
+      dateFormat(new Date('2001-01-09T00:00:00Z'), 'yyyy', tzUtc, 'en')
     ).toBe('2001')
   })
 
   it('should pad H when necessary', () => {
-    expect(
-      dateFormat(new Date('2001-01-09T01:00:00Z'), 'H', { locale: 'en' }, tzUtc)
-    ).toBe('1')
-    expect(
-      dateFormat(new Date('2001-01-19T13:00:00Z'), 'H', { locale: 'en' }, tzUtc)
-    ).toBe('13')
+    expect(dateFormat(new Date('2001-01-09T01:00:00Z'), 'H', tzUtc, 'en')).toBe(
+      '1'
+    )
+    expect(dateFormat(new Date('2001-01-19T13:00:00Z'), 'H', tzUtc, 'en')).toBe(
+      '13'
+    )
   })
 
   it('should always pad HH', () => {
     expect(
-      dateFormat(
-        new Date('2001-01-09T01:00:00Z'),
-        'HH',
-        { locale: 'en' },
-        tzUtc
-      )
+      dateFormat(new Date('2001-01-09T01:00:00Z'), 'HH', tzUtc, 'en')
     ).toBe('01')
   })
 
   it('should pad M when necessary', () => {
-    expect(
-      dateFormat(new Date('2001-01-09T01:01:00Z'), 'M', { locale: 'en' }, tzUtc)
-    ).toBe('1')
-    expect(
-      dateFormat(new Date('2001-01-19T13:13:00Z'), 'M', { locale: 'en' }, tzUtc)
-    ).toBe('13')
+    expect(dateFormat(new Date('2001-01-09T01:01:00Z'), 'M', tzUtc, 'en')).toBe(
+      '1'
+    )
+    expect(dateFormat(new Date('2001-01-19T13:13:00Z'), 'M', tzUtc, 'en')).toBe(
+      '13'
+    )
   })
 
   it('should always pad MM', () => {
     expect(
-      dateFormat(
-        new Date('2001-01-09T01:02:00Z'),
-        'MM',
-        { locale: 'en' },
-        tzUtc
-      )
+      dateFormat(new Date('2001-01-09T01:02:00Z'), 'MM', tzUtc, 'en')
     ).toBe('02')
   })
 
   it('should pad S when necessary', () => {
-    expect(
-      dateFormat(new Date('2001-01-09T01:01:05Z'), 'S', { locale: 'en' }, tzUtc)
-    ).toBe('5')
-    expect(
-      dateFormat(new Date('2001-01-19T13:13:26Z'), 'S', { locale: 'en' }, tzUtc)
-    ).toBe('26')
+    expect(dateFormat(new Date('2001-01-09T01:01:05Z'), 'S', tzUtc, 'en')).toBe(
+      '5'
+    )
+    expect(dateFormat(new Date('2001-01-19T13:13:26Z'), 'S', tzUtc, 'en')).toBe(
+      '26'
+    )
   })
 
   it('should always pad SS', () => {
-    expect(
-      dateFormat(new Date('2001-01-09T01:02:59Z'), 'S', { locale: 'en' }, tzUtc)
-    ).toBe('59')
+    expect(dateFormat(new Date('2001-01-09T01:02:59Z'), 'S', tzUtc, 'en')).toBe(
+      '59'
+    )
   })
 
   it('should get F', () => {
     expect(
-      dateFormat(
-        new Date('2001-01-09T01:02:59.123Z'),
-        'F',
-        { locale: 'en' },
-        tzUtc
-      )
+      dateFormat(new Date('2001-01-09T01:02:59.123Z'), 'F', tzUtc, 'en')
     ).toBe('1')
   })
 
   it('should get FF', () => {
     expect(
-      dateFormat(
-        new Date('2001-01-09T01:02:59.123Z'),
-        'FF',
-        { locale: 'en' },
-        tzUtc
-      )
+      dateFormat(new Date('2001-01-09T01:02:59.123Z'), 'FF', tzUtc, 'en')
     ).toBe('12')
     expect(
-      dateFormat(
-        new Date('2001-01-09T01:02:59.023Z'),
-        'FF',
-        { locale: 'en' },
-        tzUtc
-      )
+      dateFormat(new Date('2001-01-09T01:02:59.023Z'), 'FF', tzUtc, 'en')
     ).toBe('02')
   })
 
   it('should get FFF', () => {
     expect(
-      dateFormat(
-        new Date('2001-01-09T01:02:59.123Z'),
-        'FFF',
-        { locale: 'en' },
-        tzUtc
-      )
+      dateFormat(new Date('2001-01-09T01:02:59.123Z'), 'FFF', tzUtc, 'en')
     ).toBe('123')
     expect(
-      dateFormat(
-        new Date('2001-01-09T01:02:59.003Z'),
-        'FFF',
-        { locale: 'en' },
-        tzUtc
-      )
+      dateFormat(new Date('2001-01-09T01:02:59.003Z'), 'FFF', tzUtc, 'en')
     ).toBe('003')
   })
 })
