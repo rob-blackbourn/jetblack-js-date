04f585a40cf936a6c92104593903cd5fd64dace7
Rob Blackbourn <rob.blackbourn@gmail.com>
2022-08-16 14:05:27 +0100

    Added timezone to calendar test

--- test/calendar.test.ts
+++ test/calendar.test.ts
@@ -1,25 +1,25 @@
-import { calWeekends } from '../src'
+import { calWeekends, tzUtc } from '../src'
 
 describe('calendar', () => {
   describe('calWeekends', () => {
     it('should know 19 December 2014 was a Friday', () => {
       const date = new Date('2014-12-19T00:00:00Z')
-      expect(calWeekends.isWeekend(date)).toBeFalsy()
+      expect(calWeekends.isWeekend(date, tzUtc)).toBeFalsy()
     })
 
     it('should know 20 December 2014 was a Saturday', () => {
       const date = new Date('2014-12-20T00:00:00Z')
-      expect(calWeekends.isWeekend(date)).toBeTruthy()
+      expect(calWeekends.isWeekend(date, tzUtc)).toBeTruthy()
     })
 
     it('should know 21 December 2014 was a Sunday', () => {
       const date = new Date('2014-12-21T00:00:00Z')
-      expect(calWeekends.isWeekend(date)).toBeTruthy()
+      expect(calWeekends.isWeekend(date, tzUtc)).toBeTruthy()
     })
 
     it('should know 22 December 2014 was a Monday', () => {
       const date = new Date('2014-12-22T00:00:00Z')
-      expect(calWeekends.isWeekend(date)).toBeFalsy()
+      expect(calWeekends.isWeekend(date, tzUtc)).toBeFalsy()
     })
   })
 })
