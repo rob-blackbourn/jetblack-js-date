a74f4459e1e84a04a5f17cc318bb6abbf9123916
Rob Blackbourn <rob.blackbourn@gmail.com>
2023-04-20 17:51:23 +0100

    More tests

--- test/isEndOfMonth.test.ts
+++ test/isEndOfMonth.test.ts
@@ -1,28 +1,49 @@
-import { isEndOfMonth, tzUtc } from '../src'
+import {
+  IANATimezone,
+  dataToTimezoneOffset,
+  isEndOfMonth,
+  tzLocal,
+  tzUtc
+} from '../src'
+import chicagoTzData from '@jetblack/tzdata/dist/latest/America/Chicago.json'
+import tokyoTzData from '@jetblack/tzdata/dist/latest/Asia/Tokyo.json'
 
 describe('isEndOfMonth', () => {
-  it('should know 30 January is not the end of the month', () => {
-    const date = new Date('2008-01-30T00:00:00Z')
-    expect(isEndOfMonth(date, tzUtc)).toBeFalsy()
-  })
+  const tzChicago = new IANATimezone(
+    'America/Chicago',
+    chicagoTzData.map(dataToTimezoneOffset)
+  )
+  const tzTokyo = new IANATimezone(
+    'Asia/Tokyo',
+    tokyoTzData.map(dataToTimezoneOffset)
+  )
 
-  it('should know 31 January is not the end of the month', () => {
-    const date = new Date('2008-01-31T00:00:00Z')
-    expect(isEndOfMonth(date, tzUtc)).toBeTruthy()
-  })
+  for (const tz of [tzUtc, tzLocal, tzChicago, tzTokyo]) {
+    describe(tz.name, () => {
+      it('should know 30 January is not the end of the month', () => {
+        const date = tz.makeDate(2008, 0, 30)
+        expect(isEndOfMonth(date, tz)).toBeFalsy()
+      })
 
-  it('should know 28 February 2008 is not the end of the month because 2008 is a leap year', () => {
-    const date = new Date('2008-02-28T00:00:00Z')
-    expect(isEndOfMonth(date, tzUtc)).toBeFalsy()
-  })
+      it('should know 31 January is not the end of the month', () => {
+        const date = tz.makeDate(2008, 0, 31)
+        expect(isEndOfMonth(date, tz)).toBeTruthy()
+      })
 
-  it('should know 29 February 2008 is the end of the month because 2008 is a leap year', () => {
-    const date = new Date('2008-02-29T00:00:00Z')
-    expect(isEndOfMonth(date, tzUtc)).toBeTruthy()
-  })
+      it('should know 28 February 2008 is not the end of the month because 2008 is a leap year', () => {
+        const date = tz.makeDate(2008, 1, 28)
+        expect(isEndOfMonth(date, tz)).toBeFalsy()
+      })
 
-  it('should know 28 February 2009 is the end of the month because 2008 is not a leap year', () => {
-    const date = new Date('2009-02-28T00:00:00Z')
-    expect(isEndOfMonth(date, tzUtc)).toBeTruthy()
-  })
+      it('should know 29 February 2008 is the end of the month because 2008 is a leap year', () => {
+        const date = tz.makeDate(2008, 1, 29)
+        expect(isEndOfMonth(date, tz)).toBeTruthy()
+      })
+
+      it('should know 28 February 2009 is the end of the month because 2008 is not a leap year', () => {
+        const date = tz.makeDate(2009, 1, 28)
+        expect(isEndOfMonth(date, tz)).toBeTruthy()
+      })
+    })
+  }
 })
