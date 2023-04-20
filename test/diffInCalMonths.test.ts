ef7cb10cc89232fcbd8671229304ec98eccdc33f
Rob Blackbourn <rob.blackbourn@gmail.com>
2023-04-20 17:22:55 +0100

    More tests

--- test/diffInCalMonths.test.ts
+++ test/diffInCalMonths.test.ts
@@ -6,176 +6,69 @@ import {
   dataToTimezoneOffset
 } from '../src'
 import chicagoTzData from '@jetblack/tzdata/dist/latest/America/Chicago.json'
+import tokyoTzData from '@jetblack/tzdata/dist/latest/Asia/Tokyo.json'
 
 describe('diffInCalMonths', () => {
-  describe('utc', () => {
-    it('first date later', () => {
-      const actual = diffInCalMonths(
-        tzUtc.makeDate(2000, 5, 2),
-        tzUtc.makeDate(1999, 11, 2),
-        tzUtc
-      )
-      const expected = 6
-      expect(actual).toBe(expected)
-    })
+  const tzChicago = new IANATimezone(
+    'America/Chicago',
+    chicagoTzData.map(dataToTimezoneOffset)
+  )
+  const tzTokyo = new IANATimezone(
+    'Asia/Tokyo',
+    tokyoTzData.map(dataToTimezoneOffset)
+  )
 
-    it('first date earlier', () => {
-      const actual = diffInCalMonths(
-        tzUtc.makeDate(1999, 11, 2),
-        tzUtc.makeDate(2000, 5, 2),
-        tzUtc
-      )
-      const expected = -6
-      expect(actual).toBe(expected)
-    })
+  for (const tz of [tzUtc, tzLocal, tzChicago, tzTokyo]) {
+    describe(tz.name, () => {
+      it('first date later', () => {
+        const actual = diffInCalMonths(
+          tz.makeDate(2000, 5, 2),
+          tz.makeDate(1999, 11, 2),
+          tz
+        )
+        const expected = 6
+        expect(actual).toBe(expected)
+      })
 
-    it('more than a year', () => {
-      const actual = diffInCalMonths(
-        tzUtc.makeDate(2002, 5, 2),
-        tzUtc.makeDate(2000, 11, 2),
-        tzUtc
-      )
-      const expected = 18
-      expect(actual).toBe(expected)
-    })
+      it('first date earlier', () => {
+        const actual = diffInCalMonths(
+          tz.makeDate(1999, 11, 2),
+          tz.makeDate(2000, 5, 2),
+          tz
+        )
+        const expected = -6
+        expect(actual).toBe(expected)
+      })
 
-    it('over a year', () => {
-      const actual = diffInCalMonths(
-        tzUtc.makeDate(2001, 0, 1),
-        tzUtc.makeDate(2000, 11, 1),
-        tzUtc
-      )
-      const expected = 1
-      expect(actual).toBe(expected)
-    })
+      it('more than a year', () => {
+        const actual = diffInCalMonths(
+          tz.makeDate(2002, 5, 2),
+          tz.makeDate(2000, 11, 2),
+          tz
+        )
+        const expected = 18
+        expect(actual).toBe(expected)
+      })
 
-    it('just days', () => {
-      const actual = diffInCalMonths(
-        tzUtc.makeDate(2000, 1, 9),
-        tzUtc.makeDate(2000, 0, 10),
-        tzUtc
-      )
-      const expected = 0
-      expect(actual).toBe(expected)
-    })
-  })
+      it('over a year', () => {
+        const actual = diffInCalMonths(
+          tz.makeDate(2001, 0, 1),
+          tz.makeDate(2000, 11, 1),
+          tz
+        )
+        const expected = 1
+        expect(actual).toBe(expected)
+      })
 
-  describe('local', () => {
-    it('first date later', () => {
-      const actual = diffInCalMonths(
-        tzLocal.makeDate(2000, 5, 2),
-        tzLocal.makeDate(1999, 11, 2),
-        tzLocal
-      )
-      const expected = 6
-      expect(actual).toBe(expected)
+      it('just days', () => {
+        const actual = diffInCalMonths(
+          tz.makeDate(2000, 1, 9),
+          tz.makeDate(2000, 0, 10),
+          tz
+        )
+        const expected = 0
+        expect(actual).toBe(expected)
+      })
     })
-
-    it('first date earlier', () => {
-      const actual = diffInCalMonths(
-        tzLocal.makeDate(1999, 11, 2),
-        tzLocal.makeDate(2000, 5, 2),
-        tzLocal
-      )
-      const expected = -6
-      expect(actual).toBe(expected)
-    })
-
-    it('more than a year', () => {
-      const actual = diffInCalMonths(
-        tzLocal.makeDate(2002, 5, 2),
-        tzLocal.makeDate(2000, 11, 2),
-        tzLocal
-      )
-      const expected = 18
-      expect(actual).toBe(expected)
-    })
-
-    it('over a year', () => {
-      const actual = diffInCalMonths(
-        tzLocal.makeDate(2001, 0, 1),
-        tzLocal.makeDate(2000, 11, 1),
-        tzLocal
-      )
-      const expected = 1
-      expect(actual).toBe(expected)
-    })
-
-    it('just days', () => {
-      const actual = diffInCalMonths(
-        tzLocal.makeDate(2000, 1, 9),
-        tzLocal.makeDate(2000, 0, 10),
-        tzLocal
-      )
-      const expected = 0
-      expect(actual).toBe(expected)
-    })
-  })
-
-  describe('America/Chicago', () => {
-    const tzChicago = new IANATimezone(
-      'America/Chicago',
-      chicagoTzData.map(dataToTimezoneOffset)
-    )
-
-    it('should difference dates', () => {
-      const actual = diffInCalMonths(
-        tzChicago.makeDate(2000, 5, 1),
-        tzChicago.makeDate(2000, 0, 1),
-        tzChicago
-      )
-      const expected = 5
-      expect(actual).toBe(expected)
-    })
-
-    it('first date later', () => {
-      const actual = diffInCalMonths(
-        tzChicago.makeDate(2000, 5, 2),
-        tzChicago.makeDate(1999, 11, 2),
-        tzChicago
-      )
-      const expected = 6
-      expect(actual).toBe(expected)
-    })
-
-    it('first date earlier', () => {
-      const actual = diffInCalMonths(
-        tzChicago.makeDate(1999, 11, 2),
-        tzChicago.makeDate(2000, 5, 2),
-        tzChicago
-      )
-      const expected = -6
-      expect(actual).toBe(expected)
-    })
-
-    it('more than a year', () => {
-      const actual = diffInCalMonths(
-        tzChicago.makeDate(2002, 5, 2),
-        tzChicago.makeDate(2000, 11, 2),
-        tzChicago
-      )
-      const expected = 18
-      expect(actual).toBe(expected)
-    })
-
-    it('over a year', () => {
-      const actual = diffInCalMonths(
-        tzChicago.makeDate(2001, 0, 1),
-        tzChicago.makeDate(2000, 11, 1),
-        tzChicago
-      )
-      const expected = 1
-      expect(actual).toBe(expected)
-    })
-
-    it('just days', () => {
-      const actual = diffInCalMonths(
-        tzChicago.makeDate(2000, 1, 9),
-        tzChicago.makeDate(2000, 0, 10),
-        tzChicago
-      )
-      const expected = 0
-      expect(actual).toBe(expected)
-    })
-  })
+  }
 })
