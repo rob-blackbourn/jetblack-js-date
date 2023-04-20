0d8f12cf1131f18edf67c9e978d7ad4f753e3811
Rob Blackbourn <rob.blackbourn@gmail.com>
2023-04-20 17:12:27 +0100

    More tests

--- test/areInSameQuarter.test.ts
+++ test/areInSameQuarter.test.ts
@@ -1,35 +1,60 @@
-import { areInSameQuarter, tzUtc } from '../src'
+import {
+  IANATimezone,
+  areInSameQuarter,
+  dataToTimezoneOffset,
+  tzLocal,
+  tzUtc
+} from '../src'
+import chicagoTzData from '@jetblack/tzdata/dist/latest/America/Chicago.json'
+import tokyoTzData from '@jetblack/tzdata/dist/latest/Asia/Tokyo.json'
 
 describe('areInSameQuarter', () => {
-  it('should be true', () => {
-    expect(
-      areInSameQuarter(new Date('2000-01-01'), new Date('2000-03-01'), tzUtc)
-    ).toBeTruthy()
+  const tzChicago = new IANATimezone(
+    'America/Chicago',
+    chicagoTzData.map(dataToTimezoneOffset)
+  )
+  const tzTokyo = new IANATimezone(
+    'Asia/Tokyo',
+    tokyoTzData.map(dataToTimezoneOffset)
+  )
 
-    expect(
-      areInSameQuarter(new Date('2000-04-01'), new Date('2000-04-01'), tzUtc)
-    ).toBeTruthy()
+  for (const tz of [tzUtc, tzLocal, tzChicago, tzTokyo]) {
+    describe(tz.name, () => {
+      it('should be true', () => {
+        expect(
+          areInSameQuarter(tz.makeDate(2000, 0, 1), tz.makeDate(2000, 2, 1), tz)
+        ).toBeTruthy()
 
-    expect(
-      areInSameQuarter(new Date('2000-07-01'), new Date('2000-09-01'), tzUtc)
-    ).toBeTruthy()
+        expect(
+          areInSameQuarter(tz.makeDate(2000, 3, 1), tz.makeDate(2000, 3, 1), tz)
+        ).toBeTruthy()
 
-    expect(
-      areInSameQuarter(new Date('2000-10-01'), new Date('2000-12-01'), tzUtc)
-    ).toBeTruthy()
+        expect(
+          areInSameQuarter(tz.makeDate(2000, 6, 1), tz.makeDate(2000, 8, 1), tz)
+        ).toBeTruthy()
 
-    expect(
-      areInSameQuarter(new Date('2000-01-01'), new Date('2001-02-01'), tzUtc)
-    ).toBeTruthy()
-  })
+        expect(
+          areInSameQuarter(
+            tz.makeDate(2000, 9, 1),
+            tz.makeDate(2000, 11, 1),
+            tz
+          )
+        ).toBeTruthy()
 
-  it('should be false', () => {
-    expect(
-      areInSameQuarter(new Date('2000-01-01'), new Date('2000-04-01'), tzUtc)
-    ).toBeFalsy()
+        expect(
+          areInSameQuarter(tz.makeDate(2000, 0, 1), tz.makeDate(2001, 1, 1), tz)
+        ).toBeTruthy()
+      })
 
-    expect(
-      areInSameQuarter(new Date('2000-01-01'), new Date('2001-04-01'), tzUtc)
-    ).toBeFalsy()
-  })
+      it('should be false', () => {
+        expect(
+          areInSameQuarter(tz.makeDate(2000, 0, 1), tz.makeDate(2000, 3, 1), tz)
+        ).toBeFalsy()
+
+        expect(
+          areInSameQuarter(tz.makeDate(2000, 0, 1), tz.makeDate(2001, 3, 1), tz)
+        ).toBeFalsy()
+      })
+    })
+  }
 })
