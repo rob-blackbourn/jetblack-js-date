f3e8d9b0cc1afea6a75f75430bfd68164dae52ca
Rob Blackbourn <rob.blackbourn@gmail.com>
2023-04-20 07:33:30 +0100

    Updated tests

--- test/addBusinessDays.test.ts
+++ test/addBusinessDays.test.ts
@@ -1,110 +1,133 @@
-import { addBusinessDays, HolidayCalendar, calWeekends, tzUtc } from '../src'
+import {
+  IANATimezone,
+  dataToTimezoneOffset,
+  addBusinessDays,
+  HolidayCalendar,
+  calWeekends,
+  tzLocal,
+  tzUtc
+} from '../src'
+import chicagoTzData from '@jetblack/tzdata/dist/latest/America/Chicago.json'
+import tokyoTzData from '@jetblack/tzdata/dist/latest/Asia/Tokyo.json'
 
 describe('addBusinessDays', () => {
-  describe('with the default weekend calendar', () => {
-    it('should add business days within week', () => {
-      // Mon 3 Jan 2000
-      const actual = addBusinessDays(
-        new Date('2000-01-03T00:00:00Z'),
-        1,
-        calWeekends,
-        tzUtc
-      )
-      const expected = new Date('2000-01-04T00:00:00Z')
-      expect(actual.toISOString()).toBe(expected.toISOString())
-    })
+  const tzChicago = new IANATimezone(
+    'America/Chicago',
+    chicagoTzData.map(dataToTimezoneOffset)
+  )
+  const tzTokyo = new IANATimezone(
+    'Asia/Tokyo',
+    tokyoTzData.map(dataToTimezoneOffset)
+  )
 
-    it('should subtract business days within week', () => {
-      // Tue 4 Jan 2000
-      const actual = addBusinessDays(
-        new Date('2000-01-04T00:00:00Z'),
-        -1,
-        calWeekends,
-        tzUtc
-      )
-      const expected = new Date('2000-01-03T00:00:00Z')
-      expect(actual.toISOString()).toBe(expected.toISOString())
-    })
+  for (const tz of [tzUtc, tzLocal, tzChicago, tzTokyo]) {
+    describe(tz.name, () => {
+      describe('with the default weekend calendar', () => {
+        it('should add business days within week', () => {
+          // Mon 3 Jan 2000
+          const actual = addBusinessDays(
+            tz.makeDate(2000, 0, 3),
+            1,
+            calWeekends,
+            tz
+          )
+          const expected = tz.makeDate(2000, 0, 4)
+          expect(tz.toISOString(actual)).toBe(tz.toISOString(expected))
+        })
 
-    it('should add business days across a weekend', () => {
-      // Fri 7 Jan 2000
-      const actual = addBusinessDays(
-        new Date('2000-01-07T00:00:00Z'),
-        1,
-        calWeekends,
-        tzUtc
-      )
-      const expected = new Date('2000-01-10T00:00:00Z')
-      expect(actual.toISOString()).toBe(expected.toISOString())
-    })
+        it('should subtract business days within week', () => {
+          // Tue 4 Jan 2000
+          const actual = addBusinessDays(
+            tz.makeDate(2000, 0, 4),
+            -1,
+            calWeekends,
+            tz
+          )
+          const expected = tz.makeDate(2000, 0, 3)
+          expect(tz.toISOString(actual)).toBe(tz.toISOString(expected))
+        })
 
-    it('should subtract business days across a weekend', () => {
-      // Mon 10 Jan 2000
-      const actual = addBusinessDays(
-        new Date('2000-01-10T00:00:00Z'),
-        -1,
-        calWeekends,
-        tzUtc
-      )
-      const expected = new Date('2000-01-07T00:00:00Z')
-      expect(actual.toISOString()).toBe(expected.toISOString())
-    })
-  })
+        it('should add business days across a weekend', () => {
+          // Fri 7 Jan 2000
+          const actual = addBusinessDays(
+            tz.makeDate(2000, 0, 7),
+            1,
+            calWeekends,
+            tz
+          )
+          const expected = tz.makeDate(2000, 0, 10)
+          expect(tz.toISOString(actual)).toBe(tz.toISOString(expected))
+        })
 
-  describe('with custom calendar', () => {
-    const cal = new HolidayCalendar(
-      'cal',
-      [0, 6],
-      [
-        new Date('2015-01-01T00:00:00Z'),
-        new Date('2015-04-03T00:00:00Z'),
-        new Date('2015-04-06T00:00:00Z'),
-        new Date('2015-05-01T00:00:00Z'),
-        new Date('2015-12-25T00:00:00Z'),
-        new Date('2015-12-26T00:00:00Z')
-      ]
-    )
+        it('should subtract business days across a weekend', () => {
+          // Mon 10 Jan 2000
+          const actual = addBusinessDays(
+            tz.makeDate(2000, 0, 10),
+            -1,
+            calWeekends,
+            tz
+          )
+          const expected = tz.makeDate(2000, 0, 7)
+          expect(tz.toISOString(actual)).toBe(tz.toISOString(expected))
+        })
+      })
 
-    it('should add and skip New Years day', () => {
-      const date = new Date('2015-01-01T00:00:00Z')
-      const actual = addBusinessDays(date, 5, cal, tzUtc)
-      const expected = new Date('2015-01-08T00:00:00Z')
-      expect(actual.toISOString()).toBe(expected.toISOString())
-    })
+      describe('with custom calendar', () => {
+        const cal = new HolidayCalendar(
+          'cal',
+          [0, 6],
+          [
+            tz.makeDate(2015, 0, 1),
+            tz.makeDate(2015, 3, 3),
+            tz.makeDate(2015, 3, 6),
+            tz.makeDate(2015, 4, 1),
+            tz.makeDate(2015, 11, 25),
+            tz.makeDate(2015, 11, 26)
+          ]
+        )
 
-    it('should add but nothing to skip', () => {
-      const date = new Date('2015-01-02T00:00:00Z')
-      const actual = addBusinessDays(date, 4, cal, tzUtc)
-      const expected = new Date('2015-01-08T00:00:00Z')
-      expect(actual.toISOString()).toBe(expected.toISOString())
-    })
+        it('should add and skip New Years day', () => {
+          const date = tz.makeDate(2015, 0, 1)
+          const actual = addBusinessDays(date, 5, cal, tz)
+          const expected = tz.makeDate(2015, 0, 8)
+          expect(tz.toISOString(actual)).toBe(tz.toISOString(expected))
+        })
 
-    it('should add and skip', () => {
-      const date = new Date('2014-12-29T00:00:00Z')
-      const actual = addBusinessDays(date, 3, cal, tzUtc)
-      const expected = new Date('2015-01-02T00:00:00Z')
-      expect(actual.toISOString()).toBe(expected.toISOString())
-    })
+        it('should add but nothing to skip', () => {
+          const date = tz.makeDate(2015, 0, 2)
+          const actual = addBusinessDays(date, 4, cal, tz)
+          const expected = tz.makeDate(2015, 0, 8)
+          expect(tz.toISOString(actual)).toBe(tz.toISOString(expected))
+        })
 
-    it('should subtract and skip New Years day', () => {
-      const date = new Date('2015-01-08T00:00:00Z')
-      const actual = addBusinessDays(date, -5, cal, tzUtc)
-      const expected = new Date('2014-12-31T00:00:00Z')
-      expect(actual.toISOString()).toBe(expected.toISOString())
-    })
+        it('should add and skip', () => {
+          const date = tz.makeDate(2014, 11, 29)
+          const actual = addBusinessDays(date, 3, cal, tz)
+          const expected = tz.makeDate(2015, 0, 2)
+          expect(tz.toISOString(actual)).toBe(tz.toISOString(expected))
+        })
 
-    it('should subtract but nothing to skip', () => {
-      const date = new Date('2015-01-08T00:00:00Z')
-      const actual = addBusinessDays(date, -4, cal, tzUtc)
-      const expected = new Date('2015-01-02T00:00:00Z')
-      expect(actual.toISOString()).toBe(expected.toISOString())
-    })
+        it('should subtract and skip New Years day', () => {
+          const date = tz.makeDate(2015, 0, 8)
+          const actual = addBusinessDays(date, -5, cal, tz)
+          const expected = tz.makeDate(2014, 11, 31)
+          expect(tz.toISOString(actual)).toBe(tz.toISOString(expected))
+        })
 
-    it('should subtract and skip', () => {
-      const date = new Date('2015-01-02T00:00:00Z')
-      const actual = addBusinessDays(date, -2, cal, tzUtc)
-      const expected = new Date('2014-12-30T00:00:00Z')
-      expect(actual.toISOString()).toBe(expected.toISOString())
+        it('should subtract but nothing to skip', () => {
+          const date = tz.makeDate(2015, 0, 8)
+          const actual = addBusinessDays(date, -4, cal, tz)
+          const expected = tz.makeDate(2015, 0, 2)
+          expect(tz.toISOString(actual)).toBe(tz.toISOString(expected))
+        })
+
+        it('should subtract and skip', () => {
+          const date = tz.makeDate(2015, 0, 2)
+          const actual = addBusinessDays(date, -2, cal, tz)
+          const expected = tz.makeDate(2014, 11, 30)
+          expect(tz.toISOString(actual)).toBe(tz.toISOString(expected))
+        })
+      })
     })
-  })
+  }
 })
