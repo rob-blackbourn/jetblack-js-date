dd83947bc124a314dfe704196f778a397ac35de7
Rob Blackbourn <rob.blackbourn@gmail.com>
2023-04-20 07:41:55 +0100

    Updated tests

--- test/addDuration.test.ts
+++ test/addDuration.test.ts
@@ -1,25 +1,39 @@
-import { Duration, addDuration, tzUtc } from '../src'
+import {
+  IANATimezone,
+  Duration,
+  addDuration,
+  dataToTimezoneOffset,
+  tzLocal,
+  tzUtc
+} from '../src'
+import chicagoTzData from '@jetblack/tzdata/dist/latest/America/Chicago.json'
+import tokyoTzData from '@jetblack/tzdata/dist/latest/Asia/Tokyo.json'
 
 describe('addDuration', () => {
-  it('should add duration', () => {
-    const duration = new Duration('P1D')
-    const actual = addDuration(
-      new Date('2000-01-01T00:00:00Z'),
-      duration,
-      tzUtc
-    )
-    const expected = new Date('2000-01-02T00:00:00Z')
-    expect(actual.toISOString()).toBe(expected.toISOString())
-  })
+  const tzChicago = new IANATimezone(
+    'America/Chicago',
+    chicagoTzData.map(dataToTimezoneOffset)
+  )
+  const tzTokyo = new IANATimezone(
+    'Asia/Tokyo',
+    tokyoTzData.map(dataToTimezoneOffset)
+  )
 
-  it('should add a negative duration', () => {
-    const duration = new Duration('-P1D')
-    const actual = addDuration(
-      new Date('2000-01-01T00:00:00Z'),
-      duration,
-      tzUtc
-    )
-    const expected = new Date('1999-12-31T00:00:00Z')
-    expect(actual.toISOString()).toBe(expected.toISOString())
-  })
+  for (const tz of [tzUtc, tzLocal, tzChicago, tzTokyo]) {
+    describe(tz.name, () => {
+      it('should add duration', () => {
+        const duration = new Duration('P1D')
+        const actual = addDuration(tz.makeDate(2000, 0, 1), duration, tz)
+        const expected = tz.makeDate(2000, 0, 2)
+        expect(tz.toISOString(actual)).toBe(tz.toISOString(expected))
+      })
+
+      it('should add a negative duration', () => {
+        const duration = new Duration('-P1D')
+        const actual = addDuration(tz.makeDate(2000, 0, 1), duration, tz)
+        const expected = tz.makeDate(1999, 11, 31)
+        expect(tz.toISOString(actual)).toBe(tz.toISOString(expected))
+      })
+    })
+  }
 })
