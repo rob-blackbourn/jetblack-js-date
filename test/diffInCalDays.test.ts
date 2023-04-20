445e063389f7c85bcee8dda1ff72e3e777d4b485
Rob Blackbourn <rob.blackbourn@gmail.com>
2023-04-20 17:21:21 +0100

    More tests

--- test/diffInCalDays.test.ts
+++ test/diffInCalDays.test.ts
@@ -1,48 +1,53 @@
 import {
-  diffInCalDays,
-  tzUtc,
   IANATimezone,
-  dataToTimezoneOffset
+  dataToTimezoneOffset,
+  diffInCalDays,
+  tzLocal,
+  tzUtc
 } from '../src'
 import chicagoTzData from '@jetblack/tzdata/dist/latest/America/Chicago.json'
+import tokyoTzData from '@jetblack/tzdata/dist/latest/Asia/Tokyo.json'
 
 describe('diffInCalDays', () => {
-  describe('utc', () => {
-    it('should difference dates', () => {
-      const actual = diffInCalDays(
-        tzUtc.makeDate(2000, 5, 2),
-        tzUtc.makeDate(1999, 11, 21),
-        tzUtc
-      )
-      const expected = 164
-      expect(actual).toBe(expected)
-    })
-  })
+  const tzChicago = new IANATimezone(
+    'America/Chicago',
+    chicagoTzData.map(dataToTimezoneOffset)
+  )
+  const tzTokyo = new IANATimezone(
+    'Asia/Tokyo',
+    tokyoTzData.map(dataToTimezoneOffset)
+  )
 
-  describe('America/Chicago', () => {
-    const tzChicago = new IANATimezone(
-      'America/Chicago',
-      chicagoTzData.map(dataToTimezoneOffset)
+  for (const tz of [tzUtc, tzLocal, tzChicago, tzTokyo]) {
+    describe(tz.name, () => {
+      it('should difference dates across years', () => {
+        const actual = diffInCalDays(
+          tz.makeDate(2000, 5, 2),
+          tz.makeDate(1999, 11, 21),
+          tz
+        )
+        const expected = 164
+        expect(actual).toBe(expected)
+      })
+      it('should difference dates between years', () => {
+        const actual = diffInCalDays(
+          tz.makeDate(2000, 5, 2),
+          tz.makeDate(2000, 0, 1),
+          tz
+        )
+        const expected = 153
+        expect(actual).toBe(expected)
+      })
+    })
+  }
+
+  it('should handle dst', () => {
+    const actual = diffInCalDays(
+      tzChicago.makeDate(2000, 3, 2),
+      tzChicago.makeDate(2000, 2, 31, 23),
+      tzChicago
     )
-
-    it('should difference dates', () => {
-      const actual = diffInCalDays(
-        tzChicago.makeDate(2000, 5, 2),
-        tzChicago.makeDate(2000, 0, 1),
-        tzChicago
-      )
-      const expected = 153
-      expect(actual).toBe(expected)
-    })
-
-    it('should handle dst', () => {
-      const actual = diffInCalDays(
-        tzChicago.makeDate(2000, 3, 2),
-        tzChicago.makeDate(2000, 2, 31, 23),
-        tzChicago
-      )
-      const expected = 2
-      expect(actual).toBe(expected)
-    })
+    const expected = 2
+    expect(actual).toBe(expected)
   })
 })
