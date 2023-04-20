f65415acfcbe469da1ba1747eb3e5b5436a967b7
Rob Blackbourn <rob.blackbourn@gmail.com>
2023-04-20 07:15:55 +0100

    Updated test

--- test/startOfWeekday.test.ts
+++ test/startOfWeekday.test.ts
@@ -2,84 +2,67 @@ import {
   IANATimezone,
   dataToTimezoneOffset,
   startOfWeekday,
+  tzLocal,
   tzUtc
 } from '../src'
 import chicagoTzData from '@jetblack/tzdata/dist/latest/America/Chicago.json'
 
 describe('startOfWeekday', () => {
-  describe('tzUtc', () => {
-    it('Should find Sunday from Tuesday September 2 2014 at 02:11:55 with Sunday as start of week', () => {
-      const date = tzUtc.makeDate(2014, 8, 2, 2, 11, 55)
-      const actual = startOfWeekday(date, 0, tzUtc)
-      const expected = tzUtc.makeDate(2014, 7, 31)
-      expect(tzUtc.toISOString(actual)).toBe(tzUtc.toISOString(expected))
-    })
+  const tzChicago = new IANATimezone(
+    'America/Chicago',
+    chicagoTzData.map(dataToTimezoneOffset)
+  )
 
-    it('Should find Monday from Tuesday September 2 2014 at 02:11:55 with Monday as start of week', () => {
-      const date = tzUtc.makeDate(2014, 8, 2, 2, 11, 55)
-      const actual = startOfWeekday(date, 1, tzUtc)
-      const expected = tzUtc.makeDate(2014, 8, 1)
-      expect(tzUtc.toISOString(actual)).toBe(tzUtc.toISOString(expected))
-    })
+  for (const tz of [tzUtc, tzLocal, tzChicago]) {
+    describe(tz.name, () => {
+      it('Should find Sunday from Tuesday September 2 2014 at 02:11:55 with Sunday as start of week', () => {
+        const date = tz.makeDate(2014, 8, 2, 2, 11, 55)
+        const actual = startOfWeekday(date, 0, tz)
+        const expected = tz.makeDate(2014, 7, 31)
+        expect(tz.toISOString(actual)).toBe(tz.toISOString(expected))
+      })
 
-    it('Should find Tuesday from Tuesday September 2 2014 at 02:11:55 with Tuesday as start of week', () => {
-      const date = tzUtc.makeDate(2014, 8, 2, 2, 11, 55)
-      const actual = startOfWeekday(date, 2, tzUtc)
-      const expected = tzUtc.makeDate(2014, 8, 2)
-      expect(tzUtc.toISOString(actual)).toBe(tzUtc.toISOString(expected))
-    })
+      it('Should find Monday from Tuesday September 2 2014 at 02:11:55 with Monday as start of week', () => {
+        const date = tz.makeDate(2014, 8, 2, 2, 11, 55)
+        const actual = startOfWeekday(date, 1, tz)
+        const expected = tz.makeDate(2014, 8, 1)
+        expect(tz.toISOString(actual)).toBe(tz.toISOString(expected))
+      })
 
-    it('Should find Wednesday from Tuesday September 2 2014 at 02:11:55 with Wednesday as start of week', () => {
-      const date = tzUtc.makeDate(2014, 8, 2, 2, 11, 55)
-      const actual = startOfWeekday(date, 3, tzUtc)
-      const expected = tzUtc.makeDate(2014, 7, 27)
-      expect(tzUtc.toISOString(actual)).toBe(tzUtc.toISOString(expected))
-    })
+      it('Should find Tuesday from Tuesday September 2 2014 at 02:11:55 with Tuesday as start of week', () => {
+        const date = tz.makeDate(2014, 8, 2, 2, 11, 55)
+        const actual = startOfWeekday(date, 2, tz)
+        const expected = tz.makeDate(2014, 8, 2)
+        expect(tz.toISOString(actual)).toBe(tz.toISOString(expected))
+      })
 
-    it('Should find Thursday from Tuesday September 2 2014 at 02:11:55 with Thursday as start of week', () => {
-      const date = tzUtc.makeDate(2014, 8, 2, 2, 11, 55)
-      const actual = startOfWeekday(date, 4, tzUtc)
-      const expected = tzUtc.makeDate(2014, 7, 28)
-      expect(tzUtc.toISOString(actual)).toBe(tzUtc.toISOString(expected))
-    })
+      it('Should find Wednesday from Tuesday September 2 2014 at 02:11:55 with Wednesday as start of week', () => {
+        const date = tz.makeDate(2014, 8, 2, 2, 11, 55)
+        const actual = startOfWeekday(date, 3, tz)
+        const expected = tz.makeDate(2014, 7, 27)
+        expect(tz.toISOString(actual)).toBe(tz.toISOString(expected))
+      })
 
-    it('Should find Friday from Tuesday September 2 2014 at 02:11:55 with Friday as start of week', () => {
-      const date = tzUtc.makeDate(2014, 8, 2, 2, 11, 55)
-      const actual = startOfWeekday(date, 5, tzUtc)
-      const expected = tzUtc.makeDate(2014, 7, 29)
-      expect(tzUtc.toISOString(actual)).toBe(tzUtc.toISOString(expected))
-    })
+      it('Should find Thursday from Tuesday September 2 2014 at 02:11:55 with Thursday as start of week', () => {
+        const date = tz.makeDate(2014, 8, 2, 2, 11, 55)
+        const actual = startOfWeekday(date, 4, tz)
+        const expected = tz.makeDate(2014, 7, 28)
+        expect(tz.toISOString(actual)).toBe(tz.toISOString(expected))
+      })
 
-    it('Should find Saturday from Tuesday September 2 2014 at 02:11:55 with Saturday as start of week', () => {
-      const date = tzUtc.makeDate(2014, 8, 2, 2, 11, 55)
-      const actual = startOfWeekday(date, 6, tzUtc)
-      const expected = tzUtc.makeDate(2014, 7, 30)
-      expect(tzUtc.toISOString(actual)).toBe(tzUtc.toISOString(expected))
-    })
-  })
+      it('Should find Friday from Tuesday September 2 2014 at 02:11:55 with Friday as start of week', () => {
+        const date = tz.makeDate(2014, 8, 2, 2, 11, 55)
+        const actual = startOfWeekday(date, 5, tz)
+        const expected = tz.makeDate(2014, 7, 29)
+        expect(tz.toISOString(actual)).toBe(tz.toISOString(expected))
+      })
 
-  describe('tzChicago', () => {
-    const tzChicago = new IANATimezone(
-      'America/Chicago',
-      chicagoTzData.map(dataToTimezoneOffset)
-    )
-
-    it('should handle Winter', () => {
-      const date = tzChicago.makeDate(2000, 0, 1)
-      const actual = startOfWeekday(date, 0, tzChicago)
-      const expected = tzChicago.makeDate(1999, 11, 26)
-      expect(tzChicago.toISOString(actual)).toBe(
-        tzChicago.toISOString(expected)
-      )
+      it('Should find Saturday from Tuesday September 2 2014 at 02:11:55 with Saturday as start of week', () => {
+        const date = tz.makeDate(2014, 8, 2, 2, 11, 55)
+        const actual = startOfWeekday(date, 6, tz)
+        const expected = tz.makeDate(2014, 7, 30)
+        expect(tz.toISOString(actual)).toBe(tz.toISOString(expected))
+      })
     })
-
-    it('should handle summer', () => {
-      const date = tzChicago.makeDate(2000, 6, 1)
-      const actual = startOfWeekday(date, 0, tzChicago)
-      const expected = tzChicago.makeDate(2000, 5, 25)
-      expect(tzChicago.toISOString(actual)).toBe(
-        tzChicago.toISOString(expected)
-      )
-    })
-  })
+  }
 })
