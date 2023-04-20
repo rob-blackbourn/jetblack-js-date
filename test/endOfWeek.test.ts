a35cca6df94246f011c4c80763937d3d673f6234
Rob Blackbourn <rob.blackbourn@gmail.com>
2023-04-20 17:39:00 +0100

    More tests

--- test/endOfWeek.test.ts
+++ test/endOfWeek.test.ts
@@ -5,79 +5,41 @@ import {
   tzLocal,
   tzUtc
 } from '../src'
+import chicagoTzData from '@jetblack/tzdata/dist/latest/America/Chicago.json'
 import tokyoTzData from '@jetblack/tzdata/dist/latest/Asia/Tokyo.json'
 
 describe('endOfWeek', () => {
-  describe('tzUtc', () => {
-    it('should find the last moment of the week from Sunday', () => {
-      const date = tzUtc.makeDate(2022, 6, 3) // Sun 3 July 2022
-      const actual = endOfWeek(date, tzUtc)
-      const expected = tzUtc.makeDate(2022, 6, 9, 23, 59, 59, 999)
-      expect(actual.toISOString()).toBe(expected.toISOString())
-    })
+  const tzChicago = new IANATimezone(
+    'America/Chicago',
+    chicagoTzData.map(dataToTimezoneOffset)
+  )
+  const tzTokyo = new IANATimezone(
+    'Asia/Tokyo',
+    tokyoTzData.map(dataToTimezoneOffset)
+  )
 
-    it('should find the last moment of the week from Wednesday', () => {
-      const date = tzUtc.makeDate(2022, 6, 6) // Wed 56 July 2022
-      const actual = endOfWeek(date, tzUtc)
-      const expected = tzUtc.makeDate(2022, 6, 9, 23, 59, 59, 999)
-      expect(actual.toISOString()).toBe(expected.toISOString())
-    })
+  for (const tz of [tzUtc, tzLocal, tzChicago, tzTokyo]) {
+    describe(tz.name, () => {
+      it('should find the last moment of the week from Sunday', () => {
+        const date = tz.makeDate(2022, 6, 3) // Sun 3 July 2022
+        const actual = endOfWeek(date, tz)
+        const expected = tz.makeDate(2022, 6, 9, 23, 59, 59, 999)
+        expect(tz.toISOString(actual)).toBe(tz.toISOString(expected))
+      })
 
-    it('should find the last moment of the week from Saturday', () => {
-      const date = tzUtc.makeDate(2022, 6, 9)
-      const actual = endOfWeek(date, tzUtc)
-      const expected = tzUtc.makeDate(2022, 6, 9, 23, 59, 59, 999)
-      expect(actual.toISOString()).toBe(expected.toISOString())
-    })
-  })
+      it('should find the last moment of the week from Wednesday', () => {
+        const date = tz.makeDate(2022, 6, 6) // Wed 56 July 2022
+        const actual = endOfWeek(date, tz)
+        const expected = tz.makeDate(2022, 6, 9, 23, 59, 59, 999)
+        expect(tz.toISOString(actual)).toBe(tz.toISOString(expected))
+      })
 
-  describe('tzLocal', () => {
-    it('should find the last moment of the week from Sunday', () => {
-      const date = tzLocal.makeDate(2022, 6, 3) // Sun 3 July 2022
-      const actual = endOfWeek(date, tzLocal)
-      const expected = tzLocal.makeDate(2022, 6, 9, 23, 59, 59, 999)
-      expect(actual.toISOString()).toBe(expected.toISOString())
+      it('should find the last moment of the week from Saturday', () => {
+        const date = tz.makeDate(2022, 6, 9)
+        const actual = endOfWeek(date, tz)
+        const expected = tz.makeDate(2022, 6, 9, 23, 59, 59, 999)
+        expect(tz.toISOString(actual)).toBe(tz.toISOString(expected))
+      })
     })
-
-    it('should find the last moment of the week from Wednesday', () => {
-      const date = tzLocal.makeDate(2022, 6, 6) // Wed 56 July 2022
-      const actual = endOfWeek(date, tzLocal)
-      const expected = tzLocal.makeDate(2022, 6, 9, 23, 59, 59, 999)
-      expect(actual.toISOString()).toBe(expected.toISOString())
-    })
-
-    it('should find the last moment of the week from Saturday', () => {
-      const date = tzLocal.makeDate(2022, 6, 9)
-      const actual = endOfWeek(date, tzLocal)
-      const expected = tzLocal.makeDate(2022, 6, 9, 23, 59, 59, 999)
-      expect(actual.toISOString()).toBe(expected.toISOString())
-    })
-  })
-
-  describe('tzTokyo', () => {
-    const tzTokyo = new IANATimezone(
-      'Asia/Tokyo',
-      tokyoTzData.map(dataToTimezoneOffset)
-    )
-    it('should find the last moment of the week from Sunday', () => {
-      const date = tzTokyo.makeDate(2022, 6, 3) // Sun 3 July 2022
-      const actual = endOfWeek(date, tzTokyo)
-      const expected = tzTokyo.makeDate(2022, 6, 9, 23, 59, 59, 999)
-      expect(actual.toISOString()).toBe(expected.toISOString())
-    })
-
-    it('should find the last moment of the week from Wednesday', () => {
-      const date = tzTokyo.makeDate(2022, 6, 6) // Wed 56 July 2022
-      const actual = endOfWeek(date, tzTokyo)
-      const expected = tzTokyo.makeDate(2022, 6, 9, 23, 59, 59, 999)
-      expect(actual.toISOString()).toBe(expected.toISOString())
-    })
-
-    it('should find the last moment of the week from Saturday', () => {
-      const date = tzTokyo.makeDate(2022, 6, 9)
-      const actual = endOfWeek(date, tzTokyo)
-      const expected = tzTokyo.makeDate(2022, 6, 9, 23, 59, 59, 999)
-      expect(actual.toISOString()).toBe(expected.toISOString())
-    })
-  })
+  }
 })
