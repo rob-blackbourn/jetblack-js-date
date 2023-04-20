6c161fcd2ad21f1441c227f29da58a1abe87a96a
Rob Blackbourn <rob.blackbourn@gmail.com>
2023-04-20 17:07:31 +0100

    More tests

--- test/adjustBusinessDay.test.ts
+++ test/adjustBusinessDay.test.ts
@@ -1,61 +1,79 @@
 import {
-  adjustBusinessDay,
   BusinessDayConvention,
   HolidayCalendar,
+  IANATimezone,
+  adjustBusinessDay,
+  dataToTimezoneOffset,
+  tzLocal,
   tzUtc
 } from '../src'
+import chicagoTzData from '@jetblack/tzdata/dist/latest/America/Chicago.json'
+import tokyoTzData from '@jetblack/tzdata/dist/latest/Asia/Tokyo.json'
 
 describe('adjustBusinessDay', () => {
-  const cal = new HolidayCalendar(
-    'test',
-    [6, 0],
-    [
-      new Date('2015-01-01'),
-      new Date('2015-04-03'),
-      new Date('2015-04-06'),
-      new Date('2015-05-01'),
-      new Date('2015-12-25'),
-      new Date('2015-12-16')
-    ]
+  const tzChicago = new IANATimezone(
+    'America/Chicago',
+    chicagoTzData.map(dataToTimezoneOffset)
+  )
+  const tzTokyo = new IANATimezone(
+    'Asia/Tokyo',
+    tokyoTzData.map(dataToTimezoneOffset)
   )
 
-  const decThirtyFirst = new Date('2014-12-31')
-  const janFirst = new Date('2015-01-01')
-  const janSecond = new Date('2015-01-02')
+  for (const tz of [tzUtc, tzLocal, tzChicago, tzTokyo]) {
+    describe(tz.name, () => {
+      const cal = new HolidayCalendar(
+        'test',
+        [6, 0],
+        [
+          tz.makeDate(2015, 0, 1),
+          tz.makeDate(2015, 3, 3),
+          tz.makeDate(2015, 3, 6),
+          tz.makeDate(2015, 4, 1),
+          tz.makeDate(2015, 11, 25),
+          tz.makeDate(2015, 11, 16)
+        ]
+      )
 
-  it('should not require adjustment', () => {
-    expect(
-      adjustBusinessDay(
-        janFirst,
-        BusinessDayConvention.NONE,
-        true,
-        cal,
-        tzUtc
-      ).toISOString()
-    ).toBe(janFirst.toISOString())
-  })
+      const decThirtyFirst = tz.makeDate(2014, 11, 31)
+      const janFirst = tz.makeDate(2015, 0, 1)
+      const janSecond = tz.makeDate(2015, 0, 2)
 
-  it('should adjust following', () => {
-    expect(
-      adjustBusinessDay(
-        janFirst,
-        BusinessDayConvention.FOLLOWING,
-        true,
-        cal,
-        tzUtc
-      ).toISOString()
-    ).toBe(janSecond.toISOString())
-  })
+      it('should not require adjustment', () => {
+        expect(
+          adjustBusinessDay(
+            janFirst,
+            BusinessDayConvention.NONE,
+            true,
+            cal,
+            tz
+          ).toISOString()
+        ).toBe(janFirst.toISOString())
+      })
 
-  it('should adjust preceding', () => {
-    expect(
-      adjustBusinessDay(
-        janFirst,
-        BusinessDayConvention.PRECEDING,
-        true,
-        cal,
-        tzUtc
-      ).toISOString()
-    ).toBe(decThirtyFirst.toISOString())
-  })
+      it('should adjust following', () => {
+        expect(
+          adjustBusinessDay(
+            janFirst,
+            BusinessDayConvention.FOLLOWING,
+            true,
+            cal,
+            tz
+          ).toISOString()
+        ).toBe(janSecond.toISOString())
+      })
+
+      it('should adjust preceding', () => {
+        expect(
+          adjustBusinessDay(
+            janFirst,
+            BusinessDayConvention.PRECEDING,
+            true,
+            cal,
+            tz
+          ).toISOString()
+        ).toBe(decThirtyFirst.toISOString())
+      })
+    })
+  }
 })
