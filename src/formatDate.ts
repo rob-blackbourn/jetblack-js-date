c8ce91248c0f328175d8e760dee3d739a3af12c8
Rob Blackbourn <rob.blackbourn@gmail.com>
2023-04-27 06:48:45 +0100

    Pulled flags out of format function

--- src/formatDate.ts
+++ src/formatDate.ts
@@ -1,13 +1,3 @@
-/*
- * Date Format 1.2.3
- * (c) 2007-2009 Steven Levithan <stevenlevithan.com>
- * MIT license
- *
- * Includes enhancements by Scott Trenda <scott.trenda.net>
- * and Kris Kowal <cixar.com/~kris.kowal/>
- * and Rob Blackbourn.
- */
-
 import { addDays } from './addDays'
 import { isoWeekOfYear } from './isoWeekOfYear'
 import { tzLocal } from './LocalTimezone'
@@ -93,6 +83,92 @@ const getDayName = ({
   return dayName
 }
 
+const flags: Record<
+  string,
+  (date: Date, tz: Timezone, localeInfo: LocaleInfo) => any
+> = {
+  d: (date: Date, tz: Timezone, localeInfo: LocaleInfo) => tz.day(date),
+  dd: (date: Date, tz: Timezone, localeInfo: LocaleInfo) =>
+    String(tz.day(date)).padStart(2, '0'),
+  ddd: (date: Date, tz: Timezone, localeInfo: LocaleInfo) =>
+    localeInfo.weekday.short[tz.weekday(date)],
+  DD: (date: Date, tz: Timezone, localeInfo: LocaleInfo) =>
+    localeInfo.dayPlurals[tz.day(date) - 1],
+  DDD: (date: Date, tz: Timezone, localeInfo: LocaleInfo) =>
+    getDayName({
+      y: tz.year(date),
+      m: tz.monthIndex(date),
+      d: tz.day(date),
+      tz: tz,
+      dayName: localeInfo.weekday.short[tz.weekday(date)],
+      short: true
+    }),
+  dddd: (date: Date, tz: Timezone, localeInfo: LocaleInfo) =>
+    localeInfo.weekday.long[tz.weekday(date)],
+  DDDD: (date: Date, tz: Timezone, localeInfo: LocaleInfo) =>
+    getDayName({
+      y: tz.year(date),
+      m: tz.monthIndex(date),
+      d: tz.day(date),
+      tz: tz,
+      dayName: localeInfo.weekday.long[tz.weekday(date)]
+    }),
+  m: (date: Date, tz: Timezone, localeInfo: LocaleInfo) =>
+    tz.monthIndex(date) + 1,
+  mm: (date: Date, tz: Timezone, localeInfo: LocaleInfo) =>
+    String(tz.monthIndex(date) + 1).padStart(2, '0'),
+  mmm: (date: Date, tz: Timezone, localeInfo: LocaleInfo) =>
+    localeInfo.month.short[tz.monthIndex(date)],
+  mmmm: (date: Date, tz: Timezone, localeInfo: LocaleInfo) =>
+    localeInfo.month.long[tz.monthIndex(date)],
+  yy: (date: Date, tz: Timezone, localeInfo: LocaleInfo) =>
+    String(tz.year(date)).slice(2),
+  yyyy: (date: Date, tz: Timezone, localeInfo: LocaleInfo) =>
+    String(tz.year(date)).padStart(4, '0'),
+  h: (date: Date, tz: Timezone, localeInfo: LocaleInfo) =>
+    tz.hours(date) % 12 || 12,
+  hh: (date: Date, tz: Timezone, localeInfo: LocaleInfo) =>
+    String(tz.hours(date) % 12 || 12).padStart(2, '0'),
+  H: (date: Date, tz: Timezone, localeInfo: LocaleInfo) => tz.hours(date),
+  HH: (date: Date, tz: Timezone, localeInfo: LocaleInfo) =>
+    String(tz.hours(date)).padStart(2, '0'),
+  M: (date: Date, tz: Timezone, localeInfo: LocaleInfo) => tz.minutes(date),
+  MM: (date: Date, tz: Timezone, localeInfo: LocaleInfo) =>
+    String(tz.minutes(date)).padStart(2, '0'),
+  S: (date: Date, tz: Timezone, localeInfo: LocaleInfo) => tz.seconds(date),
+  SS: (date: Date, tz: Timezone, localeInfo: LocaleInfo) =>
+    String(tz.seconds(date)).padStart(2, '0'),
+  F: (date: Date, tz: Timezone, localeInfo: LocaleInfo) =>
+    String(Math.floor(tz.milliseconds(date) / 100)),
+  FF: (date: Date, tz: Timezone, localeInfo: LocaleInfo) =>
+    String(Math.floor(tz.milliseconds(date) / 10)).padStart(2, '0'),
+  FFF: (date: Date, tz: Timezone, localeInfo: LocaleInfo) =>
+    String(tz.milliseconds(date)).padStart(3, '0'),
+  t: (date: Date, tz: Timezone, localeInfo: LocaleInfo) =>
+    localeInfo.dayPeriod.narrow[tz.hours(date) < 12 ? 0 : 1],
+  tt: (date: Date, tz: Timezone, localeInfo: LocaleInfo) =>
+    localeInfo.dayPeriod.short[tz.hours(date) < 12 ? 0 : 1],
+  ttt: (date: Date, tz: Timezone, localeInfo: LocaleInfo) =>
+    localeInfo.dayPeriod.long[tz.hours(date) < 12 ? 0 : 1],
+  Z: (date: Date, tz: Timezone, localeInfo: LocaleInfo) => tz.name,
+  o: (date: Date, tz: Timezone, localeInfo: LocaleInfo) =>
+    (tz.offset(date) > 0 ? '-' : '+') +
+    String(
+      Math.floor(Math.abs(tz.offset(date)) / 60) * 100 +
+        (Math.abs(tz.offset(date)) % 60)
+    ).padStart(4, '0'),
+  p: (date: Date, tz: Timezone, localeInfo: LocaleInfo) =>
+    (tz.offset(date) > 0 ? '-' : '+') +
+    String(Math.floor(Math.abs(tz.offset(date)) / 60)).padStart(2, '0') +
+    ':' +
+    String(Math.floor(Math.abs(tz.offset(date)) % 60)).padStart(2, '0'),
+  W: (date: Date, tz: Timezone, localeInfo: LocaleInfo) => isoWeekOfYear(date),
+  WW: (date: Date, tz: Timezone, localeInfo: LocaleInfo) =>
+    String(isoWeekOfYear(date)).padStart(2, '0'),
+  N: (date: Date, tz: Timezone, localeInfo: LocaleInfo) =>
+    getDayOfWeek(date, tz)
+}
+
 /**
  * Format a date with a pattern.
  *
@@ -148,71 +224,11 @@ export function formatDate(
   tz: Timezone = tzLocal,
   locale: LocaleInfo | string | undefined = undefined
 ) {
-  const localInfo = getLocaleInfo(locale)
-
-  const flags: { [key: string]: () => any } = {
-    d: () => tz.day(date),
-    dd: () => String(tz.day(date)).padStart(2, '0'),
-    ddd: () => localInfo.weekday.short[tz.weekday(date)],
-    DD: () => localInfo.dayPlurals[tz.day(date) - 1],
-    DDD: () =>
-      getDayName({
-        y: tz.year(date),
-        m: tz.monthIndex(date),
-        d: tz.day(date),
-        tz: tz,
-        dayName: localInfo.weekday.short[tz.weekday(date)],
-        short: true
-      }),
-    dddd: () => localInfo.weekday.long[tz.weekday(date)],
-    DDDD: () =>
-      getDayName({
-        y: tz.year(date),
-        m: tz.monthIndex(date),
-        d: tz.day(date),
-        tz: tz,
-        dayName: localInfo.weekday.long[tz.weekday(date)]
-      }),
-    m: () => tz.monthIndex(date) + 1,
-    mm: () => String(tz.monthIndex(date) + 1).padStart(2, '0'),
-    mmm: () => localInfo.month.short[tz.monthIndex(date)],
-    mmmm: () => localInfo.month.long[tz.monthIndex(date)],
-    yy: () => String(tz.year(date)).slice(2),
-    yyyy: () => String(tz.year(date)).padStart(4, '0'),
-    h: () => tz.hours(date) % 12 || 12,
-    hh: () => String(tz.hours(date) % 12 || 12).padStart(2, '0'),
-    H: () => tz.hours(date),
-    HH: () => String(tz.hours(date)).padStart(2, '0'),
-    M: () => tz.minutes(date),
-    MM: () => String(tz.minutes(date)).padStart(2, '0'),
-    S: () => tz.seconds(date),
-    SS: () => String(tz.seconds(date)).padStart(2, '0'),
-    F: () => String(Math.floor(tz.milliseconds(date) / 100)),
-    FF: () => String(Math.floor(tz.milliseconds(date) / 10)).padStart(2, '0'),
-    FFF: () => String(tz.milliseconds(date)).padStart(3, '0'),
-    t: () => localInfo.dayPeriod.narrow[tz.hours(date) < 12 ? 0 : 1],
-    tt: () => localInfo.dayPeriod.short[tz.hours(date) < 12 ? 0 : 1],
-    ttt: () => localInfo.dayPeriod.long[tz.hours(date) < 12 ? 0 : 1],
-    Z: () => tz.name,
-    o: () =>
-      (tz.offset(date) > 0 ? '-' : '+') +
-      String(
-        Math.floor(Math.abs(tz.offset(date)) / 60) * 100 +
-          (Math.abs(tz.offset(date)) % 60)
-      ).padStart(4, '0'),
-    p: () =>
-      (tz.offset(date) > 0 ? '-' : '+') +
-      String(Math.floor(Math.abs(tz.offset(date)) / 60)).padStart(2, '0') +
-      ':' +
-      String(Math.floor(Math.abs(tz.offset(date)) % 60)).padStart(2, '0'),
-    W: () => isoWeekOfYear(date),
-    WW: () => String(isoWeekOfYear(date)).padStart(2, '0'),
-    N: () => getDayOfWeek(date, tz)
-  }
+  const localeInfo = getLocaleInfo(locale)
 
   return pattern.replace(token, match => {
     if (match in flags) {
-      return flags[match]()
+      return flags[match](date, tz, localeInfo)
     }
     return match.slice(1, match.length - 1)
   })
