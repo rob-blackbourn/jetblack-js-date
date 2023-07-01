ea2739e9ad8f7c99111974d33ed89b84f9b109cd
Rob Blackbourn <rob.blackbourn@gmail.com>
2023-07-01 10:13:59 +0100

    Added more docs

--- src/startOfDay.ts
+++ src/startOfDay.ts
@@ -6,6 +6,14 @@ import { Timezone } from './Timezone'
  *
  * @category Anchors
  *
+ * ```js
+ * import { startOfDay, tzLocal } from '@jetblack/date'
+ * 
+ * const date = tzLocal.makeDate(2000, 0, 1, 10, 30)
+ * console.log(tzLocal.toISOString(startOfDay(date, tzLocal)))
+ * // returns "2000-01-01T00:00:00+??:??"
+ * ```
+
  * @param date The date.
  * @param tz An optional timezone. Defaults to the local timezone.
  * @returns The start of the day.
